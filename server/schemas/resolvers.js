fb8cb39fd27d987f78c04752d1f971435720ded3
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-27 16:45:41 -0600

    added login, saveBook, and deleteBook mutations to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
-const { User, Book } = require('../models');
-const { signToken, authMiddleware } = require('../utils/auth');
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -18,6 +18,34 @@ const resolvers = {
         // login a user, sign a token, send it back
         login: async (parent, {email, password}) => {
             const user = await User.findOne({email});
+            if (!user) {
+                throw AuthenticationError;
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+            const token = signToken(user);
+            return { user, token };
+        },
+        // finds a user and updates it with the body that includes the book information
+        saveBook: async (parent, {user, body}) => {
+            return User.findOneAndUpdate(
+                { _id: user._id },
+                { $addToSet: { savedBooks: body } },
+                { new: true, runValidators: true }
+              );
+        },
+        // finds user and deletes saved book given parameters of the bookId
+        deleteBook: async (parent, {user, params}) => {
+            return User.findOneAndUpdate(
+                { _id: user._id },
+                { $pull: { savedBooks: { bookId: params.bookId } } },
+                { new: true }
+              );
         }
+
     }
-}
+};
+
+module.exports = resolvers;
