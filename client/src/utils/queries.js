31af8d168c0b372f69ffbe26c787cb1b616848c2
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-27 18:21:04 -0600

    added GET_ME query using apollo

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    query me($username: String!) {
+        user(username: $username) {
+            _id
+            username
+            email
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
+    }
+`;
