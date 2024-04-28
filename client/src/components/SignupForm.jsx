d162e1a5ffd25627f0f2d772b483cc51e0c4abdd
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-28 12:08:35 -0600

    refactored signup form with apollo/gql

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useState, useMutation } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +11,8 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  // get add user mutation
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,13 +30,13 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      // Call the addUser mutation function with userFormData as variables
+      const { data } = await addUser({
+        variables: { ...userFormData }
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
+      // Extract token and user from mutation response data
+      const { token, user } = data.addUser;
       console.log(user);
       Auth.login(token);
     } catch (err) {
