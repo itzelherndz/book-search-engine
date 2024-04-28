b353a616c2b1636d19b4732138d1f827b9888a97
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-27 18:49:48 -0600

    renamed deleteBook mutation to removeBook

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -37,7 +37,7 @@ const typeDefs = `
             link: String!
             title: String!
         ): Book
-        deleteBook(userId: ID!, bookId: ID!): User
+        removeBook(userId: ID!, bookId: ID!): User
     }
 `;
 
